ce7a39d5705dc4c03c02a444412ce5b34af6bd2f
Max Lehman <max.lehman@kellogg.northwestern.edu>
2025-05-19 20:57:57 +0000

    update

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,25 +1,26 @@
 class EntriesController < ApplicationController
 
+  def new
+    @entry = Entry.new 
+    @place = Place.find_by({"id" => params["place_id"]})
+  end
+
 
   def show
       @entry = Entry.find_by({ "id" => params["id"]})
       @place = Place.find_by({"id" => @entry["place_id"]})
   end
 
-  def new
-     @place = Place.find_by({"id" => params["place_id"]})
-  end
+
 
   def create
     @entry = Entry.new
+    @entry["title"] = params["title"]
     @entry["date"] = params["date"]
     @entry["text"] = params["text"]
-
-    @entry["place_id"] = params["company_id"]
-
+    @entry["place_id"] = params["place_id"]
     @entry.save
-
-    redirect_to "/entries/#{@entry["place_id"]}"
+    redirect_to "/places/#{params["place_id"]}"
 
   end
 end
