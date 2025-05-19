ef9c77a6aa198c2c291b939eeb1db3a6db24941d
Max Lehman <max.lehman@kellogg.northwestern.edu>
2025-05-19 22:36:52 +0000

    finished

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,26 +1,17 @@
 class EntriesController < ApplicationController
 
   def new
-    @entry = Entry.new 
-    @place = Place.find_by({"id" => params["place_id"]})
+    @entry = Entry.new
+    @place = Place.find_by({ "id"=> params["place_id"]})
   end
-
-
-  def show
-      @entry = Entry.find_by({ "id" => params["id"]})
-      @place = Place.find_by({"id" => @entry["place_id"]})
-  end
-
-
-
+  
   def create
     @entry = Entry.new
     @entry["title"] = params["title"]
-    @entry["date"] = params["date"]
-    @entry["text"] = params["text"]
+    @entry["description"] = params["description"]
+    @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
     @entry.save
     redirect_to "/places/#{params["place_id"]}"
-
+    end
   end
-end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,28 +1,22 @@
 class PlacesController < ApplicationController
 
-  def index 
+  def index
     @places = Place.all
+  end
+  
+  def show
+    @place = Place.find_by({"id"=> params["id"]})
+    @entries = Entry.where({ "place_id" => @place["id"]})
+  end
+  
+  def new
     @place = Place.new
   end
-
-  def show
-      @place = Place.find_by({ "id" => params["id"]})
-      @entries = Entry.where({"place_id" => @place["id"]})
-  end
-
-  def new
-     @place = Place.new
-  end
-
+  
   def create
     @place = Place.new
     @place["name"] = params["name"]
-
     @place.save
-
     redirect_to "/places"
-
+    end
   end
-
-
-end
